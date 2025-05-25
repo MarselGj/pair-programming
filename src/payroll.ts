44e4d6a86bdb0a66f3c53d9115546679c2a905f8
Marsel Gjergji <marselgj2008@gmail.com>
2025-05-25 21:43:12 +0200

    Payroll.ts fertig

--- src/payroll.ts
+++ src/payroll.ts
@@ -23,12 +23,53 @@ export type Payslip = {
 };
 
 export function calculatePayslip(salary: Salary): Payslip {
-  // TODO: implement
-  const result: Payslip = {
-    salary: salary,
-    deductions: new Map(),
-    totalDeductions: 0.0,
-    net: salary.gross,
+ 
+const deductions: Deductions = new Map();
+const gross = salary.gross;
+const yearsalary = salary.gross * 12;
+
+
+const birthyear = salary.born.getFullYear();
+
+// 1.Januar nach 17.Geb
+const deductionStart = new Date(salary.born.getFullYear() + 18, 0, 1)
+const Over17 = salary.payday >= deductionStart;
+
+
+const applyDeductions = (key: string, condition: boolean) => {
+if (condition){
+const rates = DEDUCTION_RATES.get(key) / 100;
+const amount = gross * rates;
+deductions.set(key, amount) 
+
+
+
+}
+};
+applyDeductions("AHV", Over17);
+applyDeductions("IV", Over17);
+applyDeductions("EO", Over17);
+
+
+applyDeductions("ALV", yearsalary >= 2500);
+applyDeductions("NBU", yearsalary >= 2500);
+
+
+applyDeductions("PK", yearsalary >= 22680);
+}
+
+let totalDeductions = 0;
+for (const amount of deduction.values())
+{
+totalDeductions += amount;
+}
+const netto = gross - totalDeductions;
+}
+
   };
   return result;
-}
+  salary,
+  deductions, 
+  totalDeductions,
+  netto,
+};
